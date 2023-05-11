25a48c497093d972f78a9120a0172ea2566edba6
peter andrew <peter.andrew987@gmail.com>
2023-05-11 13:20:41 +0700

    initialize remix + tailwind apps

---
+++ app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
